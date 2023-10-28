aa866cd99fca9aaaeaa37ad8ad5cd74d9fd4c619
shiv <developer.shivanshu@gmail.com>
2023-10-29 02:02:02 +0530

    Layout Added

---
+++ client/app/dashboard/layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from "react";
+import { MainNav } from '../../components/dashboard/main-nav'
+import { Search } from '../../components/dashboard/Search'
+import { UserNav } from '../../components/dashboard/user-nav'
+
+export default function DashboardLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    
+    return (
+        <>
+            <div className="hidden flex-col md:flex">
+                <div className="border-b">
+                    <div className="flex h-16 items-center px-4">
+                        <MainNav className="mx-6" />
+                            <div className="ml-auto flex items-center space-x-4">
+                        <Search />
+                        <UserNav />
+                        </div>
+                    </div>
+                </div>
+                {children}
+            </div>
+        </>
+    )
+  }
--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,50 +1,19 @@
-import { Metadata } from "next"
-import Image from "next/image"
+"use client"
+
+import type { ReactElement } from 'react'
 
 import { Button } from "@/components/ui/button"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 import { CalendarDateRangePicker } from "@/components/dashboard/date-range-picker"
-import { UserNav } from "@/components/dashboard/user-nav"
-import { MainNav } from "@/components/dashboard/main-nav"
 import { Overview } from "@/components/dashboard/overview"
-import { Search } from "@/components/dashboard/Search"
 
-export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Example dashboard app built using the components.",
-}
 
 export default function DashboardPage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/dashboard-light.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/dashboard-dark.png"
-          width={1280}
-          height={866}
-          alt="Dashboard"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden flex-col md:flex">
-        <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
-        </div>
+        
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
@@ -194,7 +163,6 @@ export default function DashboardPage() {
             </TabsContent>
           </Tabs>
         </div>
-      </div>
     </>
   )
 }

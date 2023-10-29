6bfa6abf0ddb53eb9543591a811bcf777e8cc7bc
shiv <developer.shivanshu@gmail.com>
2023-10-29 12:26:03 +0530

    Work

--- client/app/dashboard/layout.tsx
+++ client/app/dashboard/layout.tsx
@@ -10,19 +10,19 @@ export default function DashboardLayout({
   }) {
     
     return (
-        <>
-            <div className="hidden flex-col md:flex">
-                <div className="border-b">
-                    <div className="flex h-16 items-center px-4">
-                        <MainNav className="mx-6" />
-                            <div className="ml-auto flex items-center space-x-4">
-                        <Search />
-                        <UserNav />
-                        </div>
+        <div className="hidden flex-col md:flex">
+            <div className="border-b">
+                <div className="flex h-16 items-center px-4">
+                    <MainNav className="mx-6" />
+                        <div className="ml-auto flex items-center space-x-4">
+                    <Search />
+                    <UserNav />
                     </div>
                 </div>
+            </div>
+            <div className="container mx-auto">
                 {children}
             </div>
-        </>
+        </div>
     )
   }
